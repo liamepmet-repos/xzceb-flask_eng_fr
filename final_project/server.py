b6d83ebe1080a7db0d2fa9b8b9e73cde5a795535
liamepmet-repos <liamepmet-repos@yahoo.com>
2022-05-08 11:16:15 -0600

    return empty text message if no text is provided

--- final_project/server.py
+++ final_project/server.py
@@ -8,13 +8,19 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     french_text = translator.english_to_french(textToTranslate)
-    return french_text
+    if french_text == None:
+        return "Text is empty"
+    else:
+        return french_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     english_text = translator.french_to_english(textToTranslate)
-    return english_text
+    if english_text == None:
+        return "Text is empty"
+    else:
+        return english_text
 
 @app.route("/")
 def renderIndexPage():
